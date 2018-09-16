3f09867c91173ddddc950d99ffd71075a1e29ef3
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2018-09-15 20:48:53 -0400

    Fix degree_reqs Id

--- js/info.js
+++ js/info.js
@@ -42,7 +42,7 @@ let keywords = {
     "onecard": "one card blackboard onecard",
     "libraries": "BOW Babson Wellesley Olin Library Libraries",
     "tools": "tools machine shop",
-    "degreeReqs": "Degree course graduation major requirements",
+    "degree_reqs": "Degree course graduation major requirements",
     "sgconstitution": "SG constitution student government",
     "offcampus": "get off campus pop the bubble shuttle boston transportation",
     "mailing": "mailing lists carpediem",
